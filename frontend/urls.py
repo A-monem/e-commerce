0a8de7d22bf2d8248262065340994e8796b6ca44
Abdulmonem Nafea <abdulmonemamin@hotmail.com>
2019-07-08 03:30:16 +1000

    add 404 page not found

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-from frontend.views import index
+from django.urls import path,re_path
+from frontend.views import index, page_not_found
 
 urlpatterns = [
     path('', index, name='home'),
     path('cart/', index, name='cart'),
     path('details/', index, name='details'),
+    re_path('((.)+)/', page_not_found, name='404')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def page_not_found(request, *args, **kwargs):
+    return render(request, 'page_not_found.html')    
